4f636e037a7db6867366c4edf33ec558c2a4a341
toda-sazuki <toda.s_cha22@keio.jp>
2021-01-27 17:41:46 +0900

    課題提出ー配列

--- array/src/array/Task3.java
+++ array/src/array/Task3.java
@@ -1,32 +1,38 @@
 package array;
 
-import java.util.Random;
-
 public class Task3 {
+  public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-
-	System.out.print("[");
-
-
-	int[] array = {1,2,3,4,5,6,7,8,9};
-	Random r = new Random();
+	  System.out.print("[");
+	int[] array = new int[6];
 	for(int i=0 ; i<6 ; i++) {
-	      int a = a[r.nextInt(3)];
-	      System.out.println(a);
+		int r = new java.util.Random().nextInt(9);
+		System.out.print(r);
+		array[i] = r;
+	}
+	  System.out.println("]");
 
-	for (int i : array) {
-		if (i != 4) {
-			System.out.print(i + ",");
-		}else {
-			System.out.print(i);
+	int max = array[0];
+	for(int j = 0; j < array.length; j++) {
+		if (max <= array[j]) {
+			max = array[j];
 		}
+	}
+
+	 System.out.println("最大値:"+max);
+
+	int min = array[0];
+	for(int k = 0; k < array.length; k ++) {
+		if (min >= array[k]) {
+			min = array[k];
 		}
+		//min = Math.min(min, array[k]);
+	}
+     System.out.println("最小値:"+min);
 
-	System.out.print("]");
-
-
-
+  }
 }
-}
+
+
+
+
